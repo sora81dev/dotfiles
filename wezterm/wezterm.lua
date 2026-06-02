e5244f12d2f3f1da3eba71ec38d12ac2fa7fdd72
sora81dev <souta@prv.sora81.dev>
2026-06-03 05:17:01 +0900

    refactor wezterm: remove shorcuts unavailable in win

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -71,33 +71,6 @@ config.colors = {
 -- ショートカットキー設定
 
 config.keys = {
-	-- カーソルを一単語後ろに移動
-  {
-    key = "LeftArrow",
-    mods = "CMD",
-    action = act.SendKey {
-      key = "b",
-      mods = "META",
-    },
-  },
-  -- カーソルを一単語前に移動
-  {
-    key = "RightArrow",
-    mods = "CMD",
-    action = act.SendKey {
-      key = "f",
-      mods = "META",
-    },
-  },
--- カーソルを一単語削除
-  {
-    key = "Backspace",
-    mods = "CMD",--mac用
-    action = act.SendKey {
-      key = "w",
-      mods = "CTRL",
-   },
- },
   { key = "j", mods = "ALT", action = wezterm.action.ActivateTabRelative(-1) },
   { key = "k", mods = "ALT", action = wezterm.action.ActivateTabRelative(1) }
 }
