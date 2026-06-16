9cf254513cb2d68de30fd93c65092dcdf007ba30
sora81dev <souta@prv.sora81.dev>
2026-06-17 08:46:53 +0900

    refactor nvim: remove default config from neo-tree config

--- nvim/lua/config/neo-tree.lua
+++ nvim/lua/config/neo-tree.lua
@@ -1,10 +1,7 @@
 require('neo-tree').setup({
   filesystem = {
     filtered_items = {
-      visible = false,
       hide_dotfiles = false,
-      hide_gitignored = true,
-      hide_ignored = true
     }
   }
 })
