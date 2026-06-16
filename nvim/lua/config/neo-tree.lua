518c2f64dfe98df1a5ed772a0dd2ab8a5f700919
sora81dev <souta@prv.sora81.dev>
2026-06-17 08:45:29 +0900

    feat nvim: visible dotfiles excluding gitignored

--- nvim/lua/config/neo-tree.lua
+++ nvim/lua/config/neo-tree.lua
@@ -1,3 +1,10 @@
 require('neo-tree').setup({
-
+  filesystem = {
+    filtered_items = {
+      visible = false,
+      hide_dotfiles = false,
+      hide_gitignored = true,
+      hide_ignored = true
+    }
+  }
 })
