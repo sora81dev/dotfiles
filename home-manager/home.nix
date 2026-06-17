a01df5fd8a85b5275f2d82d8f584cd38816bc6ae
sora81dev <souta@prv.sora81.dev>
2026-06-17 10:19:04 +0900

    refactor nix-hm: remove unused comments

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -23,9 +23,6 @@
 
     # Load fonts
     pkgs.nerd-fonts.jetbrains-mono
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
