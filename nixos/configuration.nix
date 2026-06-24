b295e1e2acf4a3383bdf3905ea1b23e38d6090d5
sora81dev <117363029+sora81dev@users.noreply.github.com>
2026-06-24 12:40:42 +0900

    feat nix-hm: wezterm

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -49,11 +49,9 @@ in
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-
   # Enable the GNOME Desktop Environment.
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
-  
 
   # Configure keymap in X11
   # services.xserver.xkb.layout = "us";
@@ -95,6 +93,9 @@ in
 
     # agenix CLI
     (pkgs.callPackage "${agenixSrc}/pkgs/agenix.nix" {})
+
+    # Graphical Software
+    wezterm
   ];
 
   programs.git = {
