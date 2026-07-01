573065c0d4cecce6f3f9714c2f5e648c4a018809
Sora <117363029+sora81dev@users.noreply.github.com>
2026-07-01 14:02:06 +0900

    refactor nix: remove unused output args

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ...}@inputs: {
+  outputs = { nixpkgs, home-manager, ...}: {
     nixosConfigurations = {
       hp-notebook = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
