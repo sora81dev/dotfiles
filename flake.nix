8ac82d1b31193b76f2601fc564ab3244707443a0
sora81dev <souta@prv.sora81.dev>
2026-07-01 12:46:56 +0900

    feat nix: add new-machines configuration to flake

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Home Manager configuration of sora81dev";
+  description = "NixOS and Home Manager configuration of sora81dev";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -9,7 +9,23 @@
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations = {
+      hp-notebook = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./nixos/machines/hp-notebook/hardware-configuration.nix
+          ./nixos/configuration.nix
+
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.sora81dev = import ./home-manager/home.nix;
+          }
+        ];
+      };
+    };
     homeConfigurations."sora81dev" = home-manager.lib.homeManagerConfiguration {
       pkgs = nixpkgs.legacyPackages.x86_64-linux; # Ubuntu(64bit)の場合
 
