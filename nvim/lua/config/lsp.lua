62885ef662d1359602278111837bcd6ae4e7a420
sora81dev <souta@prv.sora81.dev>
2026-07-01 12:35:56 +0900

    feat nvim: add lsp(tailwindcss)

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -1,10 +1,11 @@
 require("mason-lspconfig").setup {
   -- Install LSP Automatically
   ensure_installed = {
-    "clangd",     -- C Language
-    "eslint",     -- Formatter
-    "ts_ls",      -- Typescript
-    "lua_ls",     -- Lua
-    "tailwindcss" -- Tailwind CSS
+    "clangd",       -- C Language
+    "eslint",       -- Formatter
+    "ts_ls",        -- Typescript
+    "lua_ls",       -- Lua
+    "tailwindcss",  -- Tailwind CSS
+    "rust_analyzer" -- Rust Analyzer
   }
 }
