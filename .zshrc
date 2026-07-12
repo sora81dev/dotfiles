69e3a59d28fc4bd5718fa98750dcee8ea2040ed4
sora81dev <117363029+sora81dev@users.noreply.github.com>
2026-07-12 21:23:01 +0900

    feat nix, zsh: use starship instead of Pure

--- .zshrc
+++ .zshrc
@@ -21,17 +21,3 @@ export GHQ_ROOT=~/ghq
 
 # GPG Key
 export GPG_TTY=$(tty)
-
-# Pure
-#   Ref: zsh用プロンプトpureを入れてみる
-#        https://hene.dev/blog/2019/07/15/pure
-RPROMPT="%{${fg[blue]}%}[%~]%{${reset_color}%}"
-autoload -Uz vcs_info
-setopt prompt_subst
-zstyle ':vcs_info:git:*' check-for-changes true
-zstyle ':vcs_info:git:*' stagedstr "%F{yellow}!"
-zstyle ':vcs_info:git:*' unstagedstr "%F{red}+"
-zstyle ':vcs_info:*' formats "%F{green}%c%u[%b]%f"
-zstyle ':vcs_info:*' actionformats '[%b|%a]'
-precmd () { vcs_info }
-RPROMPT=$RPROMPT'${vcs_info_msg_0_}'
--- nixos/home/programs/shell/default.nix
+++ nixos/home/programs/shell/default.nix
@@ -1,10 +1,11 @@
 { pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    pure-prompt
-  ];
-
   programs.zoxide = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.starship = {
     enable = true;
     enableZshIntegration = true;
   };
@@ -16,18 +17,9 @@
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
 
-    plugins = [
-    ];
-
     # Load zsh configuration file
     initExtra = ''
-      # Load original configuration
       source ~/.zshrc_custom
-
-      # Enable Pure prompt
-      fpath+=( "${pkgs.pure-prompt}/share/zsh/site-functions" )
-      autoload -U promptinit; promptinit
-      prompt pure
     '';
   };
 
