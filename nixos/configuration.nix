91e84473963df8ade81f6020d31bc05cab140698
sora81dev <117363029+sora81dev@users.noreply.github.com>
2026-07-19 23:11:44 +0900

    refactor nix: separate fonts config to configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -9,7 +9,9 @@
 }:
 
 {
-  imports = [ ];
+  imports = [
+    ./modules/fonts.nix
+  ];
 
   # boot.loader.systemd-boot.enable = true;
 
@@ -51,35 +53,6 @@
           otherlayer = { };
         };
       };
-    };
-  };
-
-  fonts = {
-    packages = with pkgs; [
-      noto-fonts
-      noto-fonts-cjk-sans
-      noto-fonts-color-emoji
-      nerd-fonts.jetbrains-mono
-      twemoji-color-font
-    ];
-
-    fontconfig.defaultFonts = {
-      serif = [
-        "Noto Serif"
-        "Noto Color Emoji"
-      ];
-      sansSerif = [
-        "Noto Sans"
-        "Noto Color Emoji"
-      ];
-      monospace = [
-        "JetBrainsMono Nerd Font"
-        "Noto Color Emoji"
-      ];
-      emoji = [
-        "Twitter Color Emoji"
-        "Noto Color Emoji"
-      ];
     };
   };
 
