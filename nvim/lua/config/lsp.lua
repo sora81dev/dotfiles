7f44cd81a410d0fa3e548cc9d8cd8c558d5790ca
sora81dev <117363029+sora81dev@users.noreply.github.com>
2026-08-07 18:59:55 +0900

    fix nvim: typo

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -85,7 +85,7 @@ vim.lsp.config("yamlls", {
       --- k8s YAML JSON Schema
       schemaStore = {
         enable = false
-      }
+      },
       schemas = {
         ["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/refs/heads/master/v1.32.1-standalone-strict/all.json"] = {
           "k8s/*.yaml",
