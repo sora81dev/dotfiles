a2fa74eb16afb212678979161d352c298cb014de
sora81dev <117363029+sora81dev@users.noreply.github.com>
2026-08-20 22:47:20 +0900

    feat nix-hm: add mpris-proxy for music control

--- nixos/home/programs/default.nix
+++ nixos/home/programs/default.nix
@@ -6,6 +6,7 @@
     ./games
     ./git
     ./latex
+    ./mpris-proxy
     ./niri
     ./nvim
     ./rofi
---
+++ nixos/home/programs/mpris-proxy/default.nix
@@ -0,0 +1,14 @@
+{ pkgs, ... }:
+{
+  services.mpris-proxy.enable = true;
+
+  # systemd.user.services.mpris-proxy = {
+  #   description = "Mpris proxy";
+  #   after = [
+  #     "network.target"
+  #     "sound.target"
+  #   ];
+  #   wantedBy = [ "default.target" ];
+  #   serviceConfig.ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
+  # };
+}
