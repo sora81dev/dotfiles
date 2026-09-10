8d14205cd05fa9dbd0eeaa12aa97f0516a5c3dbf
sora81dev <117363029+sora81dev@users.noreply.github.com>
2026-09-10 14:13:59 +0900

    feat (ghostty): add style settings

--- nixos/home/programs/ghostty/default.nix
+++ nixos/home/programs/ghostty/default.nix
@@ -8,6 +8,10 @@
     settings = {
       font-family = "JetBrainsMono Nerd Font";
       font-size = 10;
+
+      theme = "Tomorrow";
+      background = "f8f8f8";
+      window-theme = "ghostty";
     };
   };
 }
