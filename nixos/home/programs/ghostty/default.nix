01822c4ab58e60396dae66154cba1de07b2b610f
sora81dev <117363029+sora81dev@users.noreply.github.com>
2026-09-10 14:13:44 +0900

    feat (ghostty): add font settings

--- nixos/home/programs/ghostty/default.nix
+++ nixos/home/programs/ghostty/default.nix
@@ -4,5 +4,10 @@
     enable = true;
     systemd.enable = true;
     enableZshIntegration = true;
+
+    settings = {
+      font-family = "JetBrainsMono Nerd Font";
+      font-size = 10;
+    };
   };
 }
