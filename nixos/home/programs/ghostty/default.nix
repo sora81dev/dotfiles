3285ab10ac7394c68345c9080a0fc605c4a7631d
sora81dev <117363029+sora81dev@users.noreply.github.com>
2026-09-10 17:22:17 +0900

    feat (ghostty): add tab and keybind settings

--- nixos/home/programs/ghostty/default.nix
+++ nixos/home/programs/ghostty/default.nix
@@ -13,7 +13,23 @@
       background = "f8f8f8";
       window-theme = "ghostty";
 
+      keybind = [
+        "ctrl+t=new_tab"
+        "ctrl+j=previous_tab"
+        "ctrl+k=next_tab"
+        "ctrl+w=close_surface"
+      ];
+
+      gtk-tabs-location = "bottom";
+      gtk-toolbar-style = "flat";
+      gtk-wide-tabs = false;
+      window-show-tab-bar = "always";
+      window-new-tab-position = "current";
+
       gtk-single-instance = false;
+      shell-integration = "zsh";
+
+      gtk-custom-css = "~/.config/ghostty/style.css";
     };
   };
 }
